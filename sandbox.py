45b6c647e3d4ca51c012235ba1710da1bb389488
root <root@cookie@icatserver.com>
2020-07-21 20:46:12 +0700

    trial

--- sandbox.py
+++ sandbox.py
@@ -1,6 +1,8 @@
 from faker import Faker
+from faker.providers import phone_number
 
 fake_gen = Faker()
+fake_gen.add_provider(phone_number)
 
 #product_detail = '{:<13}'.format((str(
 for i in range(10):
@@ -31,6 +33,42 @@ for i in range(10):
 		for i in range(1):
 			domain_gen = '{:<21}'.format(fake_gen.domain_name())
 			print('|- %s' % (domain_gen))
-print(fake_gen.job())
+print()
 
+for i in range(10):
+	date_gen = '{:<13}'.format(fake_gen.date(pattern='%Y-%m-%d', end_datetime=None))
+	print('|- %s' % (date_gen), end =' ')
+	for i in range(1):
+		date_gen = '{:<13}'.format(fake_gen.date(pattern='%Y-%m-%d', end_datetime=None))
+		print('|- %s' % (date_gen), end =' ')
+		for i in range(1):
+			date_gen = '{:<14}'.format(fake_gen.date(pattern='%Y-%m-%d', end_datetime=None))
+			print('|- %s' % (date_gen))
+print()
+
+for i in range(10):
+	phone_gen = '{:<25}'.format(fake_gen.phone_number())[:14]
+	print('|- %s' % (phone_gen), end =' ')
+	for i in range(1):
+		phone_gen = '{:<25}'.format(fake_gen.phone_number())[:14]
+		print('|- %s' % (phone_gen), end =' ')
+		for i in range(1):
+			phone_gen = '{:<13}'.format(fake_gen.phone_number())[:14]
+			print('|- %s' % (phone_gen))
+print()
+
+for i in range(5):
+	add_gen = '{:<20}'.format(fake_gen.address())
+	if i > 0:
+		print()
+	print('Address:\n'+add_gen)
+print()
+
+for i in range(10):
+	job_gen = '{:<30}'.format(fake_gen.job())
+	print('|- %s' % (job_gen))
+print()
+
+#print(fake_gen.job())
+#print(fake_gen.address())
 
