992f17fa954d25e275dce5b3b6253a4e399f8bc5
hotpotcookie <hotpotcookie@gmail.com>
2020-08-15 00:52:39 +0700

    resolving shodan api dict

--- sandbox/sandbox.16.py
+++ sandbox/sandbox.16.py
@@ -25,74 +25,87 @@ class ShodanSeeker:
 		api = shodan.Shodan(get_key)
 		deg = u"\N{DEGREE SIGN}"
 		dnsResolve = 'https://api.shodan.io/dns/resolve?hostnames='+target+'&key='+get_key
-		try:
-			# First we need to resolve our targets domain to an IP
-			resolved = requests.get(dnsResolve)
-			hostIP = resolved.json()[target]
 
-			# Then we need to do a Shodan search on that IP
-			host    = api.host(hostIP, history=False)
-			result  = api.host(host['ip_str'])
-			countr  = 0
+		# First we need to resolve our targets domain to an IP
+		resolved = requests.get(dnsResolve)
+		hostIP = resolved.json()[target]
+
+		# Then we need to do a Shodan search on that IP
+		host    = api.host(hostIP, history=False)
+		result  = api.host(host['ip_str'])
+		port_countr = 0
+		vuln_countr = 0
+		tech_countr = 0
+		for item in host['data']:
+			port_countr += 1
+		for item in host['data'][0]['vulns']:
+			vuln_countr += 1
+		for item in host['data'][0]['http']['components']:
+			tech_countr += 1
+		try:
+			print ("\n------------------------------------------------------------------------------\nDomain Profile\n------------------------------------------------------------------------------")
+			print ("Favicon hasher\t: http.favicon.hash:%s" % host['data'][0]['hash'])
+			print ("Hostname        : %s" % host['data'][0]['http']['host'])
+			print ("IP address      : %s" % host['ip_str'])
+			print ("AS Number       : %s" % host['asn'])
+			print ("Organization    : %s" % host['org'])
 			try:
-				print ("\n------------------------------------------------------------------------------")
-				print ("Favicon hasher\t: http.favicon.hash:%s" % host['data'][0]['hash'])
-				print ("IP address      : %s" % host['ip_str'])
-				print ("Organization    : %s" % host['org'])
-				try:
-					print ("Hostname rgst   : %s" % host['domains'][0])
-				except:
-					print ("Hostname rgst   : None")
-				print ("Origin domain   : %s, %s (%s)" % (host['data'][0]['ssl']['cert']['subject']['ST'],host['country_name'], host['country_code']))
-				print ("Origin coordnt  : %s x %s" % (str(host['data'][0]['location']['longitude'])+deg, str(host['data'][0]['location']['latitude'])+deg))
-				print ("Last timestamp  : %s" % host['data'][0]['timestamp'])
-				for item in host['data']:
-					countr += 1
-				print ("Available Port  : %s" % countr)
+				print ("Hostname rgst   : %s" % host['domains'][0])
+			except:
+				abstract_ass = None
+			print ("Origin domain   : %s, %s (%s)" % (host['data'][0]['ssl']['cert']['subject']['ST'],host['country_name'], host['country_code']))
+			print ("Origin coordnt  : %s x %s" % (str(host['data'][0]['location']['longitude'])+deg, str(host['data'][0]['location']['latitude'])+deg))
+			print ("Last timestamp  : %s" % host['data'][0]['timestamp'])
+		except:
+			abstract_ass = None
+
+		# Print all technologies
+		print ("------------------------------------------------------------------------------\nTechnologies    [ %s ]\n------------------------------------------------------------------------------" % tech_countr)
+		print ("Backend Infras  : %s" % host['data'][0]['info'])
+		for item in host['data'][0]['http']['components']:
+			print ("| - %s (%s)" % (item, host['data'][0]['http']['components'][item]['categories'][0]))
+
+		# Print all banners
+		print ("------------------------------------------------------------------------------\nOpen Services   [ %s ]\n------------------------------------------------------------------------------" % port_countr)
+		for item in host['data']:
+			print ("Open Port\t: %s/%s" % (item['port'],str(item['transport']).upper()))
+			try:
+				print ("Service\t\t: %s %s" % (item['product'],item['version']))
+			except:
+				abstract_ass = None
+			try:
+				print ("Fingerprint\t: %s in %s" % (item['ssl']['dhparams']['fingerprint'], item['ssl']['dhparams']['bits']))
+				print ("SSL Versions\t: ",end="")
+				for ssl_ver in item['ssl']['versions']:
+					print (ssl_ver,end=" / ")
+				print ("\nSigntr-Algo\t: %s ver %s modulus %s bit" % (item['ssl']['cert']['sig_alg'],item['ssl']['cert']['version'],item['ssl']['cert']['pubkey']['bits']))
+				print ("Subj-Issuer\t: %s, %s (%s)" % (item['ssl']['cert']['subject']['OU'],item['ssl']['cert']['subject']['O'], host['country_code']))
+				print ("Cert-Issuer\t: %s, %s (%s)" % (item['ssl']['cert']['issuer']['O'], item['ssl']['cert']['issuer']['CN'], item['ssl']['cert']['issuer']['C']))
+				print ('')
+#				print ("Banner\t\t: \n%s" % item['data'])
 			except:
 				abstract_ass = None
 
-			# Print all banners
-			print ("------------------------------------------------------------------------------")
-			for item in host['data']:
-				print ("Open Port\t: %s/%s" % (item['port'],str(item['transport']).upper()))
-				try:
-					print ("Service\t\t: %s %s" % (item['product'],item['version']))
-				except:
-					abstract_ass = None
-				try:
-					print ("Fingerprint\t: %s in %s" % (item['ssl']['dhparams']['fingerprint'], item['ssl']['dhparams']['bits']))
-					print ("SSL Versions\t: ",end="")
-					for ssl_ver in item['ssl']['versions']:
-						print (ssl_ver,end=" / ")
-					print ("\nSigntr-Algo\t: %s version %s" % (item['ssl']['cert']['sig_alg'],item['ssl']['cert']['version']))
-					print ("Subj-Issuer\t: %s, %s" % (item['ssl']['cert']['subject']['OU'],item['ssl']['cert']['subject']['O']))
-#					print ("Banner\t\t: \n%s" % item['data'])
-				except:
-					abstract_ass = None
-
-
-		    # Print vuln information
-			print ("------------------------------------------------------------------------------")
-			for item in host['vulns']:
-#				print(host['data'][0])
-				try:
-					CVE = item.replace('!','')
-					print ('vulns: %s\t| https://www.cvedetails.com/cve/%s/' % (item, item))
-					exploits = api.exploits.search(CVE)
-#					for item in exploits['matches']:
-#						if item['cve'][0] == CVE:
-				except:
-					continue
-		except:
-			error_info = str(sys.exc_info()[1])
-			#print ('Err: '+error_info)
+	    # Print vuln information
+		parag = True
+		print ("------------------------------------------------------------------------------\nVulnerabilities [ %s ]\n------------------------------------------------------------------------------" % vuln_countr)
+		for item in host['data'][0]['vulns']:
+			if parag:
+				print ('| - [%s/10.0] %s' % (host['data'][0]['vulns'][item]['cvss'], item), end="")
+				parag = False
+				continue
+			else:
+				print ('\t| - [%s/10.0] %s' % (host['data'][0]['vulns'][item]['cvss'], item))
+				parag = True
+				continue
+#			print ('refs: https://www.cvedetails.com/cve/%s/' % item)
 
 	def shodan_seeker_proc(self):
 		target = input("Target domain\t: ")
 		print ("Initiate shodan API query for %s ..." % target)
 		#self.shodan_hash(target)
 		self.shodan_search(target)
+		print ("\n")
 
 ## main
 obj = ShodanSeeker()
